f4a9bda4b7701c8571e87d4a681d4242d90d2b3e
drag13 <no-questions@github.noreply.com>
2022-12-10 15:16:16 +0200

    data scheme simplified

--- src/results-page/results.loader.ts
+++ src/results-page/results.loader.ts
@@ -2,8 +2,21 @@ import practiceResults from "../data/log.json";
 import { calculateScoreMetric } from "../shared/math";
 
 export const practiceResultsLoader = () => {
-  return Promise.resolve(mapPracticesDto(practiceResults));
+  const results = practiceResults as PracticeResultsDto;
+  return Promise.resolve(mapPracticesDto(results));
 };
+
+type PracticeResultsDto = {
+  date: string;
+  series: {
+    distance: number;
+    ammo: string;
+    resultsMm: number[];
+    suppressor: boolean;
+    onAir: boolean;
+    position: string;
+  }[];
+}[];
 
 type PracticeResult = {
   distance: number;
@@ -12,19 +25,24 @@ type PracticeResult = {
   ammoId: string;
 };
 
-export const mapPracticesDto = (practices: unknown): PracticeResult[] => {
+export const mapPracticesDto = (
+  practices: PracticeResultsDto
+): PracticeResult[] => {
   if (!Array.isArray(practices)) {
     return [];
   }
 
   return practices
-    .map(({ groups, date }) =>
-      groups.map(({ distance, result, ammo }: any) => ({
-        distance,
-        score: calculateScoreMetric(result, distance),
-        ammoId: ammo,
-        date: new Date(date),
-      }))
+    .map(({ series, date }) =>
+      series.map(({ distance, resultsMm, ammo }) =>
+        resultsMm.map((result) => ({
+          result,
+          distance,
+          ammoId: ammo,
+          date: new Date(date),
+          score: calculateScoreMetric(result, distance),
+        }))
+      )
     )
-    .flat();
+    .flat(2);
 };
