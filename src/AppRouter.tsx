f5186faaabe8b39f4b07f4bd525238fe5fdea27b
drag13 <no-questions@github.noreply.com>
2023-01-10 11:12:34 +0200

    Added base path to the router

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { PureComponent, ReactNode } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ammoPageRoute } from "./ammo-page/ammo-page.route";
+import { BASE_PATH } from "./appconst";
 import { mmToMoaRoute } from "./calculator-page/mm-to-moa.route";
 import { Layout } from "./Layout";
 import { newLogPageRoute } from "./new-log-page/new-log-page.route";
@@ -8,19 +9,22 @@ import { logsPageRoute } from "./results-page/result-page.route";
 import { progressPageRoute } from "./stats-page/progress-page.route";
 
 const createAppRouter = () =>
-  createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        logsPageRoute,
-        mmToMoaRoute,
-        ammoPageRoute,
-        newLogPageRoute,
-        progressPageRoute,
-      ],
-    },
-  ]);
+  createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Layout />,
+        children: [
+          logsPageRoute,
+          mmToMoaRoute,
+          ammoPageRoute,
+          newLogPageRoute,
+          progressPageRoute,
+        ],
+      },
+    ],
+    { basename: BASE_PATH }
+  );
 
 export class AppRouterProvider extends PureComponent {
   render(): ReactNode {
